5ec1f2619fc3de9ecead3e88ab44db65195b38d8
cr9169 <bar.gabay.home@gmail.com>
2023-10-15 08:27:49 +0300

    feat: added SecondaryNavBar

--- front/src/components/HomePage/HomePage.tsx
+++ front/src/components/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import MainNavBar from "./MainNavBar/MainNavBar";
 import "./HomePage.scss";
+import SecondaryNavBar from "./SecondaryNavBar/SecondaryNavBar";
 
 const HomePage: React.FC = ({}: {}) => {
   const [title, setTitle] = useState("");
@@ -13,25 +14,28 @@ const HomePage: React.FC = ({}: {}) => {
   return (
     <div className="home-main-container">
       <MainNavBar />
-      <div className="home-article-container">
-        <input
-          type="text"
-          placeholder="Article Title"
-          className="home-input-title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          maxLength={30}
-        />
-        <textarea
-          placeholder="Write your content here..."
-          className="home-input-content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-        <div className="home-submit-button-container">
-          <button className="home-submit-button" onClick={handleSubmit}>
-            Submit
-          </button>
+      <div className="home-windows-section">
+          <SecondaryNavBar />
+        <div className="home-article-container">
+          <input
+            type="text"
+            placeholder="Article Title"
+            className="home-input-title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            maxLength={30}
+          />
+          <textarea
+            placeholder="Write your content here..."
+            className="home-input-content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+          <div className="home-submit-button-container">
+            <button className="home-submit-button" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
---
+++ front/src/components/HomePage/SecondaryNavBar/SecondaryNavBar.tsx
@@ -0,0 +1,7 @@
+import "./SecondaryNavBar.scss";
+
+const SecondaryNavBar = () => {
+  return <div className="secondary-nav-main-container"></div>;
+};
+
+export default SecondaryNavBar;
