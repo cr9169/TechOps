2970536e4e33e92dcc68998bc2bd8ee3abbe9ab8
cr9169 <bar.gabay.home@gmail.com>
2023-10-16 14:30:52 +0300

    feat: added functionality to input in search bar

--- front/src/components/HomePage/SearchBar/SearchBar.tsx
+++ front/src/components/HomePage/SearchBar/SearchBar.tsx
@@ -1,10 +1,23 @@
 import SearchIcon from "@mui/icons-material/Search";
 import "./SearchBar.scss";
+import { useState } from "react";
 
 function SearchBar() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="search-container">
-      <input type="text" className="search-input" placeholder="Search" />
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search"
+        value={searchValue}
+        onChange={handleInputChange}
+      />
       <button className="search-button">
         <SearchIcon className="search-icon" />
       </button>
