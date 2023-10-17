2ec67d4ca1856f9c1029054b372f69faaa7e8ab7
cr9169 <bar.gabay.home@gmail.com>
2023-10-17 15:05:30 +0300

    feat: change in interfaces

--- back/1/interface.ts
+++ back/1/interface.ts
@@ -1,8 +1,7 @@
 interface Website {
-    _id: string; // Unique identifier for the website.
-    name: string; // Name of the website.
-    url: string; // URL of the website.
-    isActive: boolean; // To check if the website is active/inactive.
-    // Other details about the website can be added as required.
-  }
-  
+  _id: string;
+  name: string;
+  url: string;
+  isActive: boolean;
+  network: string; // maybe change to enum in a more progressed stage
+}
