d8751515ed0dcca7c033d36e5f4752ee85abbf62
cr9169 <bar.gabay.home@gmail.com>
2023-11-11 00:14:32 +0200

    fix: css units

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import AuthPage from "./components/AuthPage/AuthPage";
-// import "./App.scss";
 
 function App() {
   return (
