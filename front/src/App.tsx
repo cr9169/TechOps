3602d47c09b018eaefd68e57a787cf821a60a516
cr9169 <bar.gabay.home@gmail.com>
2023-11-10 23:55:18 +0200

    fix: removed unnecessary scss

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import AuthPage from "./components/AuthPage/AuthPage";
+// import "./App.scss";
 
 function App() {
   return (
